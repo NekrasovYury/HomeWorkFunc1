db9f8cc7f9af5cba1f1480b975b6ae6b19ebd3bd
Furynoobas <samsung12345#>
2019-05-31 10:47:10 +0300

    Start

--- Func/Program.cs
+++ Func/Program.cs
@@ -9,9 +9,32 @@ namespace Func
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //Вычислить Y=F(X)
+            int a, b, c;
+            Console.WriteLine("Введите начальное значение");
+            a = Convert.ToInt32( Console.ReadLine());
+           
 
+            Console.WriteLine("Введите конечное занчение");
+            b = Convert.ToInt32( Console.ReadLine());
 
+            Console.WriteLine("Введите шаг");
+            c = Convert.ToInt32(Console.ReadLine());
+          
+
+            for (double i = a; i < b; i = i + c)
+            {
+                
+                double solution, x;
+                solution = (x * x * x) - (1 / Math.Sqrt(3) - Math.Log10(1.37 * 1.37 * 1.37 + x * x * x * x * x) + 4 / x);
+                if (solution < 0)
+                {
+                    solution = solution * -1;
+                    Console.WriteLine(solution);
+                    
+                }
+            }
+        
 
         }
     }
