c9bc7191ce350056c60bfe4a19e6f57df9601d26
Furynoobas <samsung12345#>
2019-05-31 11:43:54 +0300

    solution

--- Func/Program.cs
+++ Func/Program.cs
@@ -10,31 +10,39 @@ namespace Func
     {
         static void Main(string[] args)
         {   //Вычислить Y=F(X)
-            int a, b, c;
+            double a, b, c, SumSolution=0;
+            int k = 1;
             Console.WriteLine("Введите начальное значение");
-            a = Convert.ToInt32( Console.ReadLine());
+            a = Convert.ToDouble( Console.ReadLine());
            
 
             Console.WriteLine("Введите конечное занчение");
-            b = Convert.ToInt32( Console.ReadLine());
+            b = Convert.ToDouble( Console.ReadLine());
 
             Console.WriteLine("Введите шаг");
-            c = Convert.ToInt32(Console.ReadLine());
-          
+            c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Полученные значения");
 
             for (double i = a; i < b; i = i + c)
             {
                 
-                double solution, x;
+                double solution, x=i;
                 solution = (x * x * x) - (1 / Math.Sqrt(3) - Math.Log10(1.37 * 1.37 * 1.37 + x * x * x * x * x) + 4 / x);
                 if (solution < 0)
                 {
                     solution = solution * -1;
-                    Console.WriteLine(solution);
-                    
+                    Console.WriteLine($"{k++}: {solution}");
+                    SumSolution +=solution;
                 }
+                else
+                {
+                    Console.WriteLine($"{k++}: {solution}");
+                    SumSolution +=solution;
+                }
+                
             }
-        
+            Console.WriteLine($"Сумма всех решений: {SumSolution}");
 
         }
     }
