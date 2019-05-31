62ec68ad234799f843ba81169f2a30be6a77c95a
Furynoobas <samsung12345#>
2019-05-31 12:01:49 +0300

    Final Func

--- Func/Program.cs
+++ Func/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+
 
 namespace Func
 {
@@ -43,7 +45,11 @@ namespace Func
                 
             }
             Console.WriteLine($"Сумма всех решений: {SumSolution}");
+            Console.ReadLine();
+           
 
+            
+            }
         }
     }
 }
